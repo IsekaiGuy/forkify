7c630cf2372bee1b95d4f2b69375f3cf09be9d83
Andrey Kruglik <isekaiguy@protonmail.com>
2021-10-29 16:08:20 +0700

    Helper and config files added

--- src/js/model.js
+++ src/js/model.js
@@ -1,15 +1,16 @@
+// import dotenv from "dotenv";
+// import "dotenv/config.js";
+// dotenv.config();
+import { API_URL } from "./config";
+import { getJSON } from "./helpers";
+
 export const state = {
   recipe: {},
 };
 
 export const loadRecipe = async (id) => {
   try {
-    const res = await fetch(
-      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
-    );
-    const data = await res.json();
-
-    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+    const data = await getJSON(`${API_URL}/${id}`);
 
     const { recipe } = data.data;
     state.recipe = {
